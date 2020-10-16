74fbe9be70446f4a06bf75dfa214d93f421c157f
indy728 <kdevlinmurray@gmail.com>
2020-10-16 16:50:14 -0700

    scaffolding

---
+++ client/src/components/Checkout.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Checkout extends Component {
+  render() {
+    return <div>Check Out</div>
+  }
+}
+
+export default Checkout;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Checkout from './components/Checkout';
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route component={App} path="/" />
+      <Route component={SignIn} path="/sign-in" />
+      <Route component={SignUp} path="/sign-up" />
+      <Route component={Checkout} path="/checkout" />
+    </Switch>
+  </Router>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
