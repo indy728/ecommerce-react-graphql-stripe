bcbc674279db55053b2c70eac57e68ec6ebd5f21
indy728 <kdevlinmurray@gmail.com>
2020-10-29 11:47:20 -0700

    creds

--- server/config/database.js
+++ server/config/database.js
@@ -4,13 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: `mongodb+srv://test-rw:Happy123@cluster0.psyjk.mongodb.net/<dbname>?retryWrites=true&w=majority`
-        // host: env('DATABASE_HOST', 'cluster0.psyjk.mongodb.net'),
-        // srv: env.bool('DATABASE_SRV', true),
-        // port: env.int('DATABASE_PORT', 27017),
-        // database: env('DATABASE_NAME', 'server'),
-        // username: env('DATABASE_USERNAME', 'kyle'),
-        // password: env('DATABASE_PASSWORD', 'colmin'),
+        uri: env('MONGO_URI')
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
